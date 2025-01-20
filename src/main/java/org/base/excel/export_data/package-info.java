b23a3df8c02a3c7b16aca9a15087efb258a193a6
gengzeqing <jz_gzq@jizhi.com>
2025-01-20 13:21:28 +0800

    uodate 导出多sheet 页面

--- src/main/java/org/base/excel/export_data/package-info.java
+++ src/main/java/org/base/excel/export_data/package-info.java
@@ -7,8 +7,22 @@ package org.base.excel.export_data;
  * @MyExcelProperty 注解 指定导出的表头名称，
  *
  * 可以参考 ExcelController.export() 的导出方法
+ * ---------------------------------------------------------------------------------------------------------------------
+ *导出结果：多sheet 页面
+ *    // 创建 ExcelWriter
+ *    1.ExcelWriter writerBuilder = EasyExcel.write(outputStream).build();
  *
+ *    // 每个sheet页 创建一个sheet
+ *    2.WriteSheet build = EasyExcel.writerSheet("扣除补缴列表").registerWriteHandler(FraudUtils.exportStyle()).head(RiskFundTaskExportVo.class).build();
+ *    writerBuilder.write(taskExportVos, build);
  *
+ *    WriteSheet build1 = EasyExcel.writerSheet("扣除补缴任务明细").registerWriteHandler(FraudUtils.exportStyle()).head(RiskFundTaskDetailsExportVo.class).build();
+ *    writerBuilder.write(detailsExportVos, build1);
+ *
+ *    // 结束写入操作并关闭资源
+ *    3.writerBuilder.finish();
+ *
+ * ---------------------------------------------------------------------------------------------------------------------
  *
  *
  *
